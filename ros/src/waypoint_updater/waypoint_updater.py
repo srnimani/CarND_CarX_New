bee75f55c33e264360c676361f56c59f0cc9028e
Mani Srinivasan <mani.srinivasan@in.ibm.com>
2018-10-27 20:16:27 +0530

    TL Detector Added

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -30,6 +30,8 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
+
+        # Subscribe to the needed messages
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
